a6c5ce59fa41dc011705c947ace1bd630867563f
syedmohamed2008 <mailtosyedmohamed@gmail.com>
2026-09-24 22:48:56 +0530

    Fixed window rate limiting implemented

---
+++ RateLimiting/RateLimiting/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+
+namespace RateLimiting.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    [EnableRateLimiting("FixedPolicy")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new[] { "Laptop", "Mouse", "Keyboard" });
+        }
+    }
+}
--- RateLimiting/RateLimiting/Program.cs
+++ RateLimiting/RateLimiting/Program.cs
@@ -1,10 +1,27 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
+
+builder.Services.AddRateLimiter(options =>
+{
+    // Return 429 when the request limit is exceeded.
+    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+
+    options.AddFixedWindowLimiter("FixedPolicy", policy =>
+    {
+        policy.PermitLimit = 3;                    // Allow 3 requests
+        policy.Window = TimeSpan.FromSeconds(30); // Every 30 seconds
+        policy.QueueLimit = 0;                     // Reject extra requests
+    });
+});
+
+
 
 var app = builder.Build();
 
@@ -17,6 +34,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseRateLimiter();
 
 app.MapControllers();
 
