cf27083344fa5ad410489289f70749f5378a58b5
syedmohamed2008 <mailtosyedmohamed@gmail.com>
2026-09-24 22:53:40 +0530

    default path redis localhost:6379

--- AzureRedis/AzureRedis/Program.cs
+++ AzureRedis/AzureRedis/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 
 // One shared Redis connection manager for this application instance.
