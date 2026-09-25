2930e16db282e0c1302f0417ccab4325a6d87a4b
syedmohamed2008 <mailtosyedmohamed@gmail.com>
2026-09-25 11:06:44 +0530

    Action Level Versioning

---
+++ APIVersioning/APIVersioning/Controller/ActionLevel/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiVersioning.Controllers;
+
+[ApiController]
+[ApiVersion("1.0")]
+[ApiVersion("2.0")]
+[Route("api/v{version:apiVersion}/[controller]")]
+public class OrderController : ControllerBase
+{
+    [HttpGet]
+    [MapToApiVersion("1.0")]
+    public IActionResult GetV1()
+    {
+        return Ok(new
+        {
+            Id = 1,
+            Name = "Laptop"
+        });
+    }
+
+    [HttpGet]
+    [MapToApiVersion("2.0")]
+    public IActionResult GetV2()
+    {
+        return Ok(new
+        {
+            Id = 1,
+            Name = "Laptop",
+            Price = 50000
+        });
+    }
+}
--- APIVersioning/APIVersioning/Controller/V1/ProductsController.cs
+++ APIVersioning/APIVersioning/Controller/V1/ProductsController.cs
@@ -7,7 +7,7 @@ namespace ApiVersioning.Controllers.V1;
 [ApiVersion("1.0")]
 [Route("api/[controller]")]
 [Route("api/v{version:apiVersion}/[controller]")]
-public class ProductsController : ControllerBase
+public class OrderController : ControllerBase
 {
     [HttpGet]
     public IActionResult Get()
